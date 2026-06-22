887918d41570a9020e02c0e4440838569417d12a
ravine27 <radhaagrwal.380@gmail.com>
2026-06-22 19:37:22 +0530

    Fix websocket origin

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("http://localhost:8080")
+                .setAllowedOrigins("https://real-time-chat-room-production.up.railway.app")
                 .withSockJS();
     }
 
